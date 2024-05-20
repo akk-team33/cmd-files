cef0ef37634383089c06e287800ecc7e554f3f09
Andreas Kluge-Kaindl <akk-pg@team33.de>
2024-05-20 16:30:29 +0200

    refined Keeping, KeepingTest (step 2)

--- files-main/src/main/java/de/team33/cmd/files/main/job/Keeping.java
+++ files-main/src/main/java/de/team33/cmd/files/main/job/Keeping.java
@@ -8,12 +8,10 @@ import de.team33.patterns.io.phobos.FileEntry;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static java.util.function.Predicate.not;
@@ -21,29 +19,40 @@ import static java.util.function.Predicate.not;
 public class Keeping implements Runnable {
 
     private final Context context;
-    private final Set<String> names;
-    private final Path path2;
-    private final FileType type2;
-    private final Path path3;
+    private final Set<Path> toBeMoved;
+    private final Path movePath;
 
-    private Keeping(final Context context, final List<String> args) {
-        this.context = context;
-        this.names = names(Path.of(args.get(0)), FileType.parse(args.get(1)));
-
-        final int lastIndex;
-        if (4 > args.size()) {
-            this.path2 = Path.of(args.get(0));
-            lastIndex = 2;
-        } else {
-            this.path2 = Path.of(args.get(2));
-            lastIndex = 3;
-        }
-        this.type2 = FileType.parse(args.get(lastIndex));
-        this.path3 = path2.resolve("(moved)");
+    private Keeping(final Context context, final String path, final String type1, final String type2) {
+        this(context, path, type1, path, type2);
     }
 
-    private static Set<String> names(final Path path, final FileType type) {
-        return FileEntry.evaluated(path)
+    private Keeping(final Context context,
+                    final String path1, final String type1,
+                    final String path2, final String type2) {
+        this(context, Path.of(path1), FileType.parse(type1), Path.of(path2), FileType.parse(type2));
+    }
+
+    private Keeping(final Context context,
+                    final Path path1, final FileType type1,
+                    final Path path2, final FileType type2) {
+        final Set<String> nameToKeep = pureNamesOf(path1, type1);
+        context.printf("[names to keep]%n%s%n", String.format(String.join("%n", nameToKeep)));
+        this.context = context;
+        this.toBeMoved = toBeMoved(path2, type2, nameToKeep);
+        this.movePath = path2.resolve("(moved)");
+    }
+
+    private static Set<Path> toBeMoved(final Path parent, final FileType type, final Set<String> names) {
+        return FileEntry.evaluated(parent)
+                        .content()
+                        .stream()
+                        .filter(type::isTypeOf)
+                        .filter(not(path -> isMatching(path, names)))
+                        .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private static Set<String> pureNamesOf(final Path parent, final FileType type) {
+        return FileEntry.evaluated(parent)
                         .content()
                         .stream()
                         .filter(Files::isRegularFile)
@@ -52,52 +61,33 @@ public class Keeping implements Runnable {
                         .collect(Collectors.toCollection(TreeSet::new));
     }
 
-    private static boolean isType(final Set<String> type, final Path path) {
-        final String fullName = path.getFileName().toString().toLowerCase();
-        return type.stream()
-                   .map(ext -> "." + ext.toLowerCase())
-                   .anyMatch(fullName::endsWith);
-    }
-
-    private static Set<String> setOf(final String csv) {
-        return new TreeSet<>(Arrays.asList(csv.split(",")));
-    }
-
     public static Runnable job(final Context context, final List<String> args) {
         assert 1 < args.size();
         assert Regular.KEEP.name().equalsIgnoreCase(args.get(1));
         // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        return job(context, args.get(0), args);
-    }
-
-    private static Runnable job(final Context context, final String cmdName, final List<String> args) {
         final int size = args.size();
-        if (5 <= size && size <= 6) {
-            return new Keeping(context, args.subList(2, size));
+        if (5 == size) {
+            return new Keeping(context, args.get(2), args.get(3), args.get(4));
+        } else if (6 == size) {
+            return new Keeping(context, args.get(2), args.get(3), args.get(4), args.get(5));
         } else {
             final String format = TextIO.read(Keeping.class, "Keeping.txt");
             final String cmdLine = String.join(" ", args);
-            return () -> context.printf(format, cmdLine, cmdName);
+            return () -> context.printf(format, cmdLine, args.get(0));
         }
     }
 
     @Override
     public final void run() {
-        FileEntry.evaluated(path2)
-                 .content()
-                 .stream()
-                 .filter(type2::isTypeOf)
-                 .filter(not(path -> isMatching(path, names)))
-                 //.collect(Collectors.toList())
-                 .forEach(this::move);
+        toBeMoved.forEach(this::move);
     }
 
     private void move(final Path path) {
         final Path fileName = path.getFileName();
-        final Path target = path3.resolve(fileName);
+        final Path target = movePath.resolve(fileName);
         context.printf("moving %s%n-> %s ...", path, target);
         try {
-            Files.createDirectories(path3);
+            Files.createDirectories(movePath);
             Files.move(path, target);
             context.printf(" ok%n");
         } catch (final IOException e) {
