36a2e6ebede8a4803b85821b9963b92360ec4b63
Kluge-Kaindl, Andreas <andreas.klugekaindl@governikus.de>
2024-09-17 09:58:35 +0200

    Removed superfluous files

--- files-main/src/main/java/de/team33/cmd/files/main/common/Context.java
+++
@@ -1,8 +0,0 @@
-package de.team33.cmd.files.main.common;
-
-public interface Context {
-
-    default void printf(final String format, final Object... args) {
-        System.out.printf(format, args);
-    }
-}
