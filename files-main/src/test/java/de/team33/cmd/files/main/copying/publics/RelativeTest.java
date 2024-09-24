6a58a4c61968542a1a5dfb5e83f05a6e578896dd
Andreas Kluge-Kaindl <akk-pg@team33.de>
2024-09-24 18:04:59 +0200

    Prepared Relative and RelativeTest (step 2)

--- files-main/src/test/java/de/team33/cmd/files/main/copying/publics/RelativeTest.java
+++ files-main/src/test/java/de/team33/cmd/files/main/copying/publics/RelativeTest.java
@@ -4,10 +4,14 @@ import de.team33.cmd.files.main.copying.Relative;
 import de.team33.patterns.io.deimos.TextIO;
 import de.team33.testing.io.hydra.FileInfo;
 import de.team33.testing.io.hydra.ZipIO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.util.Set;
 import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,12 +20,15 @@ class RelativeTest {
     private static final String CLASS_NAME = RelativeTest.class.getSimpleName();
     private static final Path TEST_PATH = Path.of("target", "testing", CLASS_NAME);
 
-    private final String uuid = UUID.randomUUID().toString();
-    private final Path mainPath = TEST_PATH.resolve(uuid);
-    private final Path leftPath = mainPath.resolve("left");
-    private final Path rightPath = mainPath.resolve("right");
+    private Path leftPath;
+    private Path rightPath;
 
-    {
+    @BeforeEach
+    final void init() {
+        final Path testPath = TEST_PATH.resolve(UUID.randomUUID().toString());
+        leftPath = testPath.resolve("left");
+        rightPath = testPath.resolve("right");
+
         ZipIO.unzip(Relative.class, "leftFiles.zip", leftPath);
         assertEquals(TextIO.read(Relative.class, "leftFiles.txt"),
                      FileInfo.of(leftPath).toString());
@@ -32,6 +39,12 @@ class RelativeTest {
 
     @Test
     final void test() {
-        //Relative
+        final Set<Path> expected = TextIO.read(RelativeTest.class, "RelativeTest-collect.txt")
+                                         .lines()
+                                         .filter(Predicate.not(String::isBlank))
+                                         .map(Path::of)
+                                         .collect(Collectors.toSet());
+        final Set<Path> result = Relative.collect(leftPath, rightPath);
+        assertEquals(expected, result);
     }
 }
