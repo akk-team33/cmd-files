48abc9eaebbdf3ec61fc8b7776df997ecb664ff7
Kluge-Kaindl, Andreas <andreas.klugekaindl@governikus.de>
2024-09-30 15:32:50 +0200

    renamed Counter -> Index in WildcardString

--- files-main/src/main/java/de/team33/cmd/files/main/finder/WildcardString.java
+++ files-main/src/main/java/de/team33/cmd/files/main/finder/WildcardString.java
@@ -2,12 +2,11 @@ package de.team33.cmd.files.main.finder;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import static java.util.function.Predicate.*;
+import static java.util.function.Predicate.not;
 
 class WildcardString {
 
@@ -16,13 +15,13 @@ class WildcardString {
     public static String parse(final String origin) {
         final Matcher matcher = WILDCARD.matcher(origin);
         final List<String> result = new LinkedList<>();
-        final Counter counter = new Counter();
+        final Index index = new Index();
         matcher.results().forEach(matchResult -> {
             final int start = matchResult.start();
-            result.add(origin.substring(counter.value, start));
-            result.add(origin.substring(start, counter.value = matchResult.end()));
+            result.add(origin.substring(index.value, start));
+            result.add(origin.substring(start, index.value = matchResult.end()));
         });
-        result.add(origin.substring(counter.value));
+        result.add(origin.substring(index.value));
         return result.stream()
                      .filter(not(String::isEmpty))
                      .map(subs -> switch (subs) {
@@ -33,7 +32,7 @@ class WildcardString {
                      .collect(Collectors.joining());
     }
 
-    private static class Counter {
+    private static class Index {
         private int value = 0;
     }
 }
