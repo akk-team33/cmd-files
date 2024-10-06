40c7d6bc9c887e201c38b291739a0caa07ed6057
Kluge-Kaindl, Andreas <andreas.klugekaindl@governikus.de>
2024-10-06 13:43:20 +0200

    Introducing TypeMatcher (dirty)

---
+++ files-main/src/main/java/de/team33/cmd/files/finding/TypeMatcher.java
@@ -0,0 +1,62 @@
+package de.team33.cmd.files.finding;
+
+import de.team33.patterns.io.alpha.FileEntry;
+import de.team33.patterns.io.alpha.FileType;
+import de.team33.patterns.io.deimos.TextIO;
+
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TypeMatcher {
+
+    private final Set<String> extensions;
+    private final Set<FileType> fileTypes;
+
+    private TypeMatcher(final Set<String> extensions, final Set<FileType> fileTypes) {
+        this.extensions = extensions;
+        this.fileTypes = fileTypes;
+    }
+
+    /**
+     * @throws IllegalArgumentException if <em>pattern</em> is invalid.
+     */
+    public static TypeMatcher parse(final String pattern) {
+        try {
+            return parseEx(pattern);
+        } catch (final InternalException e) {
+            final String message = TextIO.read(NameMatcher.class, "TypeMatcher.txt")
+                                         .formatted(pattern, e.getMessage());
+            throw new IllegalArgumentException(message, e);
+        }
+    }
+
+    private static TypeMatcher parseEx(final String pattern) throws InternalException {
+        final String[] parts = pattern.split(":", -1);
+        if (1 == parts.length) {
+            return parseEx(parts[0], "");
+        } else if (2 == parts.length) {
+            return parseEx(parts[0], parts[1]);
+        } else {
+            throw new InternalException("TODO ... must not contain ':' (a colon)!");
+        }
+    }
+
+    private static TypeMatcher parseEx(final String extensions, final String fileTypes) throws InternalException {
+        final Set<String> exts = Stream.of(extensions.split(","))
+                                       .map(String::toLowerCase)
+                                       .collect(Collectors.toSet());
+        final Set<FileType> types = EnumSet.allOf(FileType.class); // TODO
+        return new TypeMatcher(exts, types);
+    }
+
+    public final boolean matches(final FileEntry entry) {
+        return extensions.contains(extensionOf(entry.path().getFileName().toString()));
+    }
+
+    private String extensionOf(final String fileName) {
+        final int index = fileName.lastIndexOf('.');
+        return (0 > index) ? null : fileName.substring(index + 1).toLowerCase();
+    }
+}
---
+++ files-main/src/test/java/de/team33/cmd/files/finding/publics/TypeMatcherTest.java
@@ -0,0 +1,42 @@
+package de.team33.cmd.files.finding.publics;
+
+import de.team33.cmd.files.finding.TypeMatcher;
+import de.team33.patterns.io.alpha.FileEntry;
+import de.team33.patterns.io.alpha.FilePolicy;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TypeMatcherTest {
+
+    private FileEntry entry(final String path) {
+        return FileEntry.of(Path.of(path), FilePolicy.DISTINCT_SYMLINKS);
+    }
+
+    @Test
+    final void parse_extensions() {
+        final TypeMatcher matcher = TypeMatcher.parse("jpg,JPE,jPEg");
+        assertTrue(matcher.matches(entry("myImage.Jpg")));
+        assertTrue(matcher.matches(entry("myImage.jPe")));
+        assertTrue(matcher.matches(entry("myImage.jpEg")));
+        assertFalse(matcher.matches(entry("myImage")));
+        assertFalse(matcher.matches(entry("myImage.tif")));
+        assertFalse(matcher.matches(entry("myImage.tiff")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {""})
+    final void parse_fail(final String pattern) {
+        try {
+            final TypeMatcher matcher = TypeMatcher.parse(pattern);
+            fail("expected to fail - but was " + matcher);
+        } catch (final IllegalArgumentException e) {
+            // as expected -> OK!
+            e.printStackTrace();
+        }
+    }
+}
