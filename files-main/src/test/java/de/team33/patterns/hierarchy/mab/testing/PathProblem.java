20ce3aa54f3d63a4143a382ef23292b2e4107e75
Andi Kluge <akk-dev@team33.de>
2026-06-30 21:21:26 +0200

    [4.x-directory.iocaste] removed tests for package <hierarchy.mab> to fix problems

--- files-main/src/test/java/de/team33/patterns/hierarchy/mab/testing/PathProblem.java
+++
@@ -1,9 +0,0 @@
-package de.team33.patterns.hierarchy.mab.testing;
-
-import de.team33.patterns.hierarchy.mab.Nodes;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-public record PathProblem(Path node, IOException cause) implements Nodes.Problem<Path> {
-}
