d0f8376a7eb27bbd3a7be64abf001ba8e2449896
Andi Kluge <akk-dev@team33.de>
2026-07-21 04:52:34 +0200

    [4.x-config-00] introduced JsonMapper & JsonParser (io-gamma) - dirty - reading is green

--- files-main/src/main/java/de/team33/patterns/io/gamma/json/JsonMapper.java
+++ files-main/src/main/java/de/team33/patterns/io/gamma/json/JsonMapper.java
@@ -1,6 +1,15 @@
 package de.team33.patterns.io.gamma.json;
 
+import de.team33.patterns.enums.pan.Values;
+
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.RecordComponent;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class JsonMapper<T extends Record> {
 
@@ -16,8 +25,118 @@ public class JsonMapper<T extends Record> {
         return new JsonMapper<>(recordClass, source).map();
     }
 
-    private T map() throws IOException {
-        final JsonObject value = JsonParser.parse(source);
-        throw new UnsupportedOperationException("not yet implemented");
+    private static Object mapRecord(final Class<?> recordClass, final JsonObject jsonObject) {
+        final RecordComponent[] recordComponents = recordClass.getRecordComponents();
+        final Object[] initials = map(jsonObject, recordComponents);
+        final Class<?>[] types = Stream.of(recordComponents)
+                                       .map(RecordComponent::getType)
+                                       .toArray(Class<?>[]::new);
+        try {
+            final Constructor<?> constructor = recordClass.getDeclaredConstructor(types);
+            constructor.setAccessible(true);
+            return constructor.newInstance(initials);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static Object[] map(final JsonObject jsonObject, final RecordComponent[] recordComponents) {
+        final Object[] result = new Object[recordComponents.length];
+        for (int index = 0; index < recordComponents.length; ++index) {
+            final RecordComponent recordComponent = recordComponents[index];
+            final String name = recordComponent.getName();
+            result[index] = map(recordComponent.getType(), jsonObject.get(name));
+        }
+        return result;
+    }
+
+    private static Object map(final Class<?> type, final JsonValue jsonValue) {
+        if (JsonValue.NULL == jsonValue) {
+            return null;
+        } else {
+            return mapNonNull(type, jsonValue);
+        }
+    }
+
+    private static Object mapNonNull(final Class<?> type, final JsonValue jsonValue) {
+        if (type.isRecord()) {
+            return mapRecord(type, (JsonObject) jsonValue);
+        } else {
+            return mapNoRecord(type, jsonValue);
+        }
+    }
+
+    private static Object mapNoRecord(final Class<?> type, final JsonValue jsonValue) {
+        return switch (Mappable.of(type)) {
+            case BOOLEAN -> ((JsonBoolean) jsonValue).value();
+            case BYTE -> ((JsonNumber) jsonValue).value().byteValueExact();
+            case SHORT -> ((JsonNumber) jsonValue).value().shortValueExact();
+            case INT -> ((JsonNumber) jsonValue).value().intValueExact();
+            case LONG -> ((JsonNumber) jsonValue).value().longValueExact();
+            case FLOAT -> ((JsonNumber) jsonValue).value().floatValue();
+            case DOUBLE -> ((JsonNumber) jsonValue).value().doubleValue();
+            case BIG_INTEGER -> ((JsonNumber) jsonValue).value().toBigIntegerExact();
+            case BIG_DECIMAL -> ((JsonNumber) jsonValue).value();
+            case STRING -> ((JsonString) jsonValue).value();
+            case ENUM -> enumValueOf(type, ((JsonString) jsonValue).value());
+            case OTHER -> fail(type);
+        };
+    }
+
+    private static Object enumValueOf(final Class<?> type, final String value) {
+        return Stream.of(type.getEnumConstants())
+                     .map(Enum.class::cast)
+                     .filter(e -> e.name().equals(value))
+                     .findAny()
+                     .map(Object.class::cast)
+                     .orElseGet(() -> fail(type));
+    }
+
+    private static Object fail(final Class<?> type) {
+        throw new IllegalStateException("cannot map <%s>".formatted(type.getCanonicalName()));
+    }
+
+    private T map() {
+        final JsonValue value = JsonParser.parse(source);
+        if (value instanceof JsonObject jsonObject) {
+            return recordClass.cast(mapRecord(recordClass, jsonObject));
+        } else {
+            throw new IllegalArgumentException(
+                    "expected JsonObject - but was %s".formatted(value.getClass().getSimpleName()));
+        }
+    }
+
+    private enum Mappable {
+
+        BOOLEAN(boolean.class, Boolean.class),
+        BYTE(byte.class, Byte.class),
+        SHORT(short.class, Short.class),
+        INT(int.class, Integer.class),
+        LONG(long.class, Long.class),
+        FLOAT(float.class, Float.class),
+        DOUBLE(double.class, Double.class),
+        BIG_INTEGER(BigInteger.class),
+        BIG_DECIMAL(BigDecimal.class),
+        STRING(String.class),
+        ENUM(),
+        OTHER();
+
+        private static final Values<Mappable> VALUES = Values.of(Mappable.class);
+
+        private final Set<Class<?>> classes;
+
+        Mappable(final Class<?>... classes) {
+            this.classes = Set.of(classes);
+        }
+
+        public static Mappable of(final Class<?> type) {
+            return VALUES.findAny(value -> value.classes.contains(type))
+                         .orElseGet(() -> ofOther(type));
+        }
+
+        private static Mappable ofOther(final Class<?> type) {
+            return type.isEnum() ? ENUM : OTHER;
+        }
     }
 }
--- files-main/src/main/java/de/team33/patterns/io/gamma/json/JsonParser.java
+++ files-main/src/main/java/de/team33/patterns/io/gamma/json/JsonParser.java
@@ -1,64 +1,197 @@
 package de.team33.patterns.io.gamma.json;
 
+import java.math.BigDecimal;
+
 class JsonParser {
 
-    private final String source;
+    private static final String LIMIT_CHARS = ",}]";
 
-    private int start;
+    private final String source;
+    private int index;
 
     private JsonParser(final String source) {
         this.source = source;
-        this.start = 0;
+        this.index = 0;
     }
 
-    static JsonObject parse(final String source) {
+    static JsonValue parse(final String source) {
         return new JsonParser(source).parseRoot();
     }
 
-    private JsonObject parseRoot() {
+    private JsonValue parseRoot() {
         skipWhitespace();
-        testNotEOT();
-        final JsonObject result = source.charAt(start) == '{' ? parseObject() : fail();
+        final JsonValue result = parseValue();
         testEOT();
         return result;
     }
 
+    private JsonValue parseValue() {
+        testNotEOT();
+        return switch (source.charAt(index)) {
+            case '{' -> parseObject();
+            case '[' -> parseArray();
+            case '"' -> parseString();
+            case 't', 'T', 'f', 'F' -> parseBoolean();
+            default -> parseNumber();
+        };
+    }
+
     private JsonObject parseObject() {
-        // starts with '{' ...
-        start += 1;
+        skipExpected('{');
         skipWhitespace();
         final JsonObject.Builder builder = parseObjectBody();
-        if (source.charAt(start) == '}') {
-            start += 1;
-            skipWhitespace();
-            return builder.build();
-        }
-        throw new UnsupportedOperationException("not yet implemented");
+        skipExpected('}');
+        skipWhitespace();
+        return builder.build();
     }
 
     private JsonObject.Builder parseObjectBody() {
+        final JsonObject.Builder builder = JsonObject.builder();
+        char next = isEOT() ? 0 : ',';
+        while ((',' == next)) {
+            final String name = parseString().value();
+            skipExpected(':');
+            skipWhitespace();
+            final var value = parseValue();
+            builder.put(name, value);
+            next = isEOT() ? 0 : source.charAt(index);
+            if (',' == next) {
+                index += 1;
+                skipWhitespace();
+            }
+        }
+        return builder;
+    }
+
+    private JsonValue parseArray() {
         throw new UnsupportedOperationException("not yet implemented");
     }
 
-    private <T> T fail() {
-        throw new IllegalArgumentException("unexpected char at %d: '%c'".formatted(start, source.charAt(start)));
+    private JsonString parseString() {
+        final StringLiteral stage = new StringLiteral();
+        return new JsonString(stage.body());
+    }
+
+    private void skipExpected(final char expected) {
+        final char c = source.charAt(index);
+        if (expected == c) {
+            index += 1;
+        } else {
+            throw new IllegalArgumentException(
+                    "expected '%c' - but was '%c' at index %d".formatted(expected, c, index));
+        }
+    }
+
+    private JsonBoolean parseBoolean() {
+        final int limit = findLimit();
+        final boolean value = Boolean.parseBoolean(source.substring(index, limit));
+        index = limit;
+        skipWhitespace();
+        return new JsonBoolean(value);
+    }
+
+    private JsonNumber parseNumber() {
+        final int limit = findLimit();
+        final BigDecimal number = new BigDecimal(source.substring(index, limit));
+        index = limit;
+        skipWhitespace();
+        return new JsonNumber(number);
+    }
+
+    private int findLimit() {
+        int next = index + 1;
+        while (!isLimitIndex(next)) {
+            next += 1;
+        }
+        return next;
+    }
+
+    private boolean isLimitIndex(final int next) {
+        return isEOT() || isLimitChar(source.charAt(next));
+    }
+
+    private boolean isLimitChar(final char c) {
+        return Character.isWhitespace(c) || (0 <= LIMIT_CHARS.indexOf(c));
     }
 
     private void testEOT() {
-        if (start < source.length()) {
-            throw new IllegalArgumentException("unexpected text at index %d".formatted(start));
+        if (!isEOT()) {
+            throw new IllegalArgumentException("expected end of source text at index %d".formatted(index));
         }
     }
 
     private void testNotEOT() {
-        if (start >= source.length()) {
-            throw new IllegalArgumentException("unexpected end of source text at index %d".formatted(start));
+        if (isEOT()) {
+            throw new IllegalArgumentException("unexpected end of source text at index %d".formatted(index));
         }
     }
 
+    private boolean isEOT() {
+        return index >= source.length();
+    }
+
     private void skipWhitespace() {
-        while (Character.isWhitespace(source.charAt(start))) {
-            start += 1;
+        while (index < source.length() && Character.isWhitespace(source.charAt(index))) {
+            index += 1;
+        }
+    }
+
+    private class CharLiteral {
+
+        private int value;
+
+        CharLiteral() {
+            this.value = isEOT() ? -1 : source.charAt(index);
+            if ('"' == value) {
+                this.value = -1;
+            } else if ('\\' == value) {
+                this.value = escChar();
+            }
+            index += ((0 > value) ? 0 : 1);
+        }
+
+        private int escChar() {
+            index += 1;
+            testNotEOT();
+            final char next = source.charAt(index);
+            return switch (next) {
+                case '\\' -> '\\';
+                case '"' -> '"';
+                case 'b' -> '\b';
+                case 'f' -> '\f';
+                case 'n' -> '\n';
+                case 'r' -> '\r';
+                case 't' -> '\t';
+                default -> -1;
+            };
+        }
+
+        final boolean isPresent() {
+            return 0 <= value;
+        }
+
+        final char toChar() {
+            return (char) value;
+        }
+    }
+
+    private class StringLiteral {
+
+        private final StringBuilder body = new StringBuilder();
+
+        StringLiteral() {
+            skipExpected('"');
+            CharLiteral next = new CharLiteral();
+            while (next.isPresent()) {
+                body.append(next.toChar());
+                next = new CharLiteral();
+            }
+            skipExpected('"');
+            skipWhitespace();
+        }
+
+        final String body() {
+            return body.toString();
         }
     }
 }
